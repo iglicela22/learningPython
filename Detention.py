9bdbec58595de51def9f0aa6503b21f480cb721f
Igli Cela <iglicela222@gmail.com>
2025-05-31 12:00:12 +0200

    updated_comments

---
+++ Detention.py
@@ -0,0 +1,6 @@
+# Detention
+
+# Loop from 0 to 100 (inclusive) => 101 total repetitions
+for i in range(101):
+    # Print the sentence as punishment/detention line
+    print('I will not use Snapchat in class')
